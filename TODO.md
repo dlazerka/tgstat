3a264ccfe9f6e01f334dec74a9ccf2be7c4f3ac4
Dzmitry Lazerka <dlazerka@gmail.com>
2022-08-22 23:41:49 -0700

    Listening for http requests

--- TODO.md
+++ TODO.md
@@ -2,7 +2,7 @@
 * Telegram bot should count all messages.
 
 ## Tasks
-* Create a bot in telegram
-* Make any http request to telegram API
+* + Create a bot in telegram
+* + Make any http request to telegram API
 * Get messages count using getUpdates()
 * Rewrite getUpdates to server.
--- src/com/victoriametrics/tgstat/Main.java
+++ src/com/victoriametrics/tgstat/Main.java
@@ -1,26 +1,28 @@
 package com.victoriametrics.tgstat;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
 
-        String token = readToken();
-
-        makeApiCall(token, "/getMe");
-
-        makeApiCall(token, "/getUpdates");
-    }
-
-    private static String makeApiCall(String token, String apiCall) throws IOException {
+    private static String makeApiCall(
+            String token,
+            String apiCall
+    ) throws IOException {
         System.out.println("Sending: " + apiCall);
 
         URL url = new URL("https://api.telegram.org/bot" + token + apiCall);
+        System.out.println("Sending: " + url);
+
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestMethod("GET");
         con.setDoOutput(true);
@@ -48,5 +50,25 @@ public class Main {
         String token = new String(bytes).trim();
         System.out.println(token);
         return token;
+    }
+
+    public static void main(String[] args) throws IOException {
+        // String token = readToken();
+        // makeApiCall(token, "/getMe");
+        // makeApiCall(token, "/getUpdates");
+        // makeApiCall(token, "/sendMessage?chat_id=388268832&text=yo");
+
+        try (ServerSocket server = new ServerSocket(8888)) {
+
+            Socket conn = server.accept();
+
+            InputStream inputStream = conn.getInputStream();
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+            System.out.println(bufferedReader.readLine());
+
+            conn.close();
+        }
     }
 }
